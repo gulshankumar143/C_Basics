473c950cfb1aae33821bf4d9f227c62ca2f6be36
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-02 00:02:27 +0530

    Int_Data_Type

---
+++ Data Type/Int.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   int x=9;
+   int y=2;
+   printf("%d\n",x+y);
+   printf("%d\n",x-y);
+   printf("%d\n",x*y);
+   printf("%d\n",x/y);
+   return 0;
+}
+/*
+Output:
+11
+7
+18
+4
+*/
---
+++ Data Type/int1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int w,z,a,b;
+   int x=9;
+   int y=2;
+   z=x+y;
+   w=x-y;
+   a=x*y;
+   b=x/y;
+   printf("%d\n",z);
+   printf("%d\n",w);
+   printf("%d\n",a);
+   printf("%d\n",b);
+   return 0;
+}
+/*
+Output:
+11
+7
+18
+4*/
