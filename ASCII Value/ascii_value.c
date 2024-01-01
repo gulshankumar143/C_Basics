c68ea5d6548bc16d22303e77b36fddd94d34a4e0
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-01 23:59:22 +0530

    First Commit

---
+++ ASCII Value/ascii_value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char ch = 'A';
+    char ch1 = 'Z';
+    char ch2 ='a';
+    char ch3 = 'z';
+    printf("%d\t%d\t%d\t%d",ch,ch1,ch2,ch3);
+    return 0;
+}
+/*
+Output:
+65      90      97      122
+*/
---
+++ ASCII Value/exclamation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char ch = '!';
+    printf("%d",ch);
+    return 0;
+}
+/*
+Output:
+33
+*/
