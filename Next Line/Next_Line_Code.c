fd5bb23a481eae2910b095de7df84ef77e609f90
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-02 00:01:17 +0530

    First Commit

---
+++ Next Line/Next_Line_Code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World \n");
+    printf("Hello to C-Programming");
+    return 0;
+}
+/*
+Output:
+Hello World
+Hello to C-Programming
+*/
---
+++ Next Line/Next_Line_Code1.c
@@ -0,0 +1,13 @@
+//Escape Sequence '\n'
+//Generally used to write something in the next line
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\nHello to C-Programming");
+    return 0;
+}
+/*
+Output:
+Hello World
+Hello to C-Programming
+*/
