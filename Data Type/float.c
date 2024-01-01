5fa63010d4df0784cafca2e8c6b43ef26b6be2d8
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-02 00:00:12 +0530

    Float Data type

---
+++ Data Type/float.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float x=9;
+    float y=2;
+    float a=x/y;
+    printf("%f",a);
+    return 0;
+}
+/*
+Output:
+4.500000
+*/
---
+++ Data Type/float_example.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i=2,j=3,k,l;
+    float a,b;
+    k=i/j*j;
+    l=j/i*i;
+    a=i/j*j;
+    b=j/i*i;
+    printf("%d %d %f %f",k,l,a,b);
+    return 0;
+}
+/*
+Output:
+0 2 0.000000 2.000000
+*/
