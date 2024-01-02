bedc51954da45878770191d28d828c0973e77040
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-03 00:57:59 +0530

    First Commit

---
+++ Data Type/boolean2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=65;
+    char j='A'; //Here it gives the ASCII Value of 'A' which is 65.
+    if(i==j)
+        printf("C is Awesome.");
+    else
+        printf("C is headache.");
+        return 0;
+}
+
+/*
+C is Awesome.
+*/
